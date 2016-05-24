586e7ae46b8e43f56caa936ea736aa70df259d27
dmitr-nik <dmitr_nik@yahoo.com>
2016-05-24 17:28:00 +0300

    Add

---
+++ SysSetupFormRun/Methods/ClassDeclaration.cpp
@@ -0,0 +1,16 @@
+// This is a framework class. Customizing this class may cause problems with future upgrades to the software.
+public class SysSetupFormRun extends FormRun
+{
+    SysWorkflowFormControls     workflowControls;
+    // <GEEU>
+    // Event listeners list
+    List listeners_W;
+    // </GEEU>
+
+    ltcFormMethodHandler      formMethodHandler;  //ltc, LiteTreeClassifier, don
+#if.never
+    #define.CurrentVersion(1)
+    #localmacro.CurrentList
+    #endmacro
+#endif
+}
